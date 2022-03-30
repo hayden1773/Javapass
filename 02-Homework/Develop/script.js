da71a47361a20bbaf8bc8e1a27ffd2e6e6ce0e7f
Hayden Kiltoff <hayden060@gmail.com>
2022-03-29 22:15:44 -0700

    added HTML,CSS,JAVA,Readme

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,20 @@
+function generatePassword(){
+  //TODO: your code goes here
+  return "password"
+}
+
+
+// Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
