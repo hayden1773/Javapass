5757cd070e5966ea11a45b573a7993568ce90988
Hayden Kiltoff <hayden060@gmail.com>
2022-04-03 09:49:41 -0700

    javascript updated loop

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-function generatePassword(){
+function generatePassword() {
   //TODO: your code goes here
-  return "password"
+
+  var prompt1 = window.confirm("Uppercase?")
+  var prompt2 = window.confirm("lowercase?")
+  var prompt3 = window.confirm("number?")
+  var prompt4 = window.confirm("special character?")
+  var prompt5 = parseInt(window.prompt("how many characters"))
+
+  if (prompt5 >= 8 && prompt5 <= 128) {
+
+    console.log("confirm")
+
+    if (prompt1 == true || prompt2 == true || prompt3 == true || prompt4 == true) {
+      console.log("confirm")
+
+      var passwordOptions = []
+      if (prompt1 == true) {
+        for (var index = 0; index < upperCase.length; index++) {
+          passwordOptions.push(upperCase[index])
+
+        }
+      }
+      if (prompt2 == true) {
+        for (var index = 0; index < lowerCase.length; index++) {
+          passwordOptions.push(lowerCase[index])
+
+        }
+      }
+      if (prompt3 == true) {
+        for (var index = 0; index < number.length; index++) {
+          passwordOptions.push(number[index])
+
+        }
+      }
+      if (prompt4 == true) {
+        for (var index = 0; index < special.length; index++) {
+          passwordOptions.push(special[index])
+
+        }
+      }
+      console.log(passwordOptions)
+      var password = ""
+      for (var index = 0; index < prompt5; index++) {
+        var randomCharacter = passwordOptions[Math.floor(Math.random() * passwordOptions.length)];
+        password = password + randomCharacter
+        console.log(password)
+      }
+      return password
+    }
+
+  }
+  alert("password criteria not met, please keep between 8 and 128 characters")
+
 }
 
-var upperCase = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
-var lowerCase = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
-var number = ['1','2','3','4','5','6','7','8','9']
-var special = ['!','@','#','$','%','^','&','*','+','=','-','?']
+var upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+var lowerCase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '=', '-', '?']
 // GIVEN I need a new, secure password
 
 // WHEN I click the button to generate a password
@@ -14,18 +65,18 @@ var special = ['!','@','#','$','%','^','&','*','+','=','-','?']
 // THEN I am presented with a series of prompts for password criteria
 
 // WHEN prompted for password criteria
-promt1= window.confirm ("Uppercase?")
-promt2= window.confirm ("lowercase?")
-prompt3= window.confirm ("number?")
-prompt4= window.confirm ("special character?")
-prompt5= window.confirm ("how many characters")
+// promt1= window.confirm ("Uppercase?")
+// promt2= window.confirm ("lowercase?")
+// prompt3= window.confirm ("number?")
+// prompt4= window.confirm ("special character?")
+// prompt5= window.confirm ("how many characters")
 // THEN I select which criteria to include in the password
 
-  if (prompt1 == prompt1);{
-    upperCase = Math.floor(Math.random* upperCase.length)
-    console.log ("confirm")
-    
-  }
+// if (prompt1 == prompt1);{
+//   upperCase = Math.floor(Math.random* upperCase.length)
+//   console.log ("confirm")
+
+// }
 
 
 // WHEN prompted for the length of the password
